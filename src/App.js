677360fe7df14f240c996b540c0a75452c4a5a90
hnthinh2909 <hnthinh2909@gmail.com>
2020-08-23 13:21:35 +0700

    Add onClick to remove TodoList

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.scss";
 import ColorBox from "./components/ColorBox";
+import TodoList from "./components/TodoList";
 
 function App() {
+    const [todoList, setTodoList] = useState([
+        { id: 1, title: "I love you!" },
+        { id: 2, title: "I love you babe!" },
+        { id: 3, title: "I love u!" },
+    ]);
+
+    const onTodoClick = (todo) => {
+        console.log(todo);
+        const index = todoList.findIndex((x) => x.id === todo.id);
+        if (index < 0) return;
+        const newTodoList = [...todoList];
+        newTodoList.splice(index, 1);
+        setTodoList(newTodoList);
+    };
+
     return (
         <div className="app">
             <h1>
                 Welcome to React Hooks!
                 <ColorBox />
+                <TodoList todos={todoList} onTodoClick={onTodoClick} />
             </h1>
         </div>
     );
